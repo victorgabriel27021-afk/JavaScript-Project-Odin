37fe8dde811c64a90c4c9cab39e9cad380727ad0
Victor Gabriel <victorgabriel27021@gmail.com>
2026-02-24 15:06:11 -0300

    Javascript Project rock, paper or scissor finish 2

--- java.js
+++ java.js
@@ -1,56 +1,55 @@
 function getComputerChoice() {
-	const random = Math.floor(Math.random() * 3);
+const random = Math.floor(Math.random() * 3);
 
-	if (random === 0) {
-		return "rock";
+if (random === 0) {
+	return "rock";
 	} else if (random === 1) {
-		return "paper";
+	return "paper";
 	} else {
-		return "scissors";
+	return "scissors";
 	}
 }
 
 function getHumanChoice() {
-	return prompt("rock, paper or scissors").toLowerCase();
+  return prompt("rock, paper or scissors").toLowerCase();
 }
 
 function playGame() {
+let HumanScore = 0;
+let ComputerScore = 0;
 
-	let HumanScore = 0;
-	let ComputerScore = 0;
+function playRound(HumanChoice, ComputerChoice) {
 
-	function playRound(HumanChoice, ComputerChoice) {
+if (HumanChoice === ComputerChoice) {
+	console.log("DRAW! Try Again");
+}
+else if (
+	(HumanChoice === "rock" && ComputerChoice === "scissors") ||
+	(HumanChoice === "paper" && ComputerChoice === "rock") ||
+	(HumanChoice === "scissors" && ComputerChoice === "paper")
+) {
+	Console.log(`YOU WIN!!! ${HumanChoice} beats ${ComputerChoice}`);
+	HumanScore++;
+}
+else {
+	console.log(`YOU LOSE! ${ComputerChoice} beats ${HumanChoice}`);
+	ComputerScore++;
+  }
+}
 
-		if (HumanChoice === ComputerChoice) {
-			console.log("DRAW! Try Again");
-		}
-		else if (
-			(HumanChoice === "rock" && ComputerChoice === "scissors") ||
-			(HumanChoice === "paper" && ComputerChoice === "rock") ||
-			(HumanChoice === "scissors" && ComputerChoice === "paper")
-		) {
-			console.log(`YOU WIN!!! ${HumanChoice} beats ${ComputerChoice}`);
-			HumanScore++;
-		}
-		else {
-			console.log(`YOU LOSE! ${ComputerChoice} beats ${HumanChoice}`);
-			ComputerScore++;
-		}
-	}
-
-	for (let count = 0; count < 5; count++) {
-		let humanSelection = getHumanChoice();
-		let computerSelection = getComputerChoice();
-		playRound(humanSelection, computerSelection);
-	}
+for (let count = 0; count < 5; count++) {
+	let humanSelection = getHumanChoice();
+	let computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+}
 
 	if (HumanScore > ComputerScore) {
-		console.log(`YOU WIN THE GAME! ${HumanScore} x ${ComputerScore}`);
-	} else if (ComputerScore > HumanScore) {
-		console.log(`YOU LOSE THE GAME! ${ComputerScore} x ${HumanScore}`);
-	} else {
-		console.log(`DRAW GAME! ${HumanScore} x ${ComputerScore}`);
-	}
+	console.log(`YOU WIN THE GAME! ${HumanScore} x ${ComputerScore}`);
+} else if (ComputerScore > HumanScore) {
+	console.log(`YOU LOSE THE GAME! ${ComputerScore} x ${HumanScore}`);
+} else {
+	console.log(`DRAW GAME! ${HumanScore} x ${ComputerScore}`);
+  }
 }
 
 playGame();
